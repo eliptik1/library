c16713fe2a2b4bd6c6709a9d6b3541a4b885251c
eliptik <adamfeyk5@gmail.com>
2023-04-09 18:49:40 +0300

    Create displayBooks function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let myBook = [{"Title":"Atomic Habits","Author":"James Clear"}];
+let myBook = [{"Title":"Atomic Habits","Author":"James Clear"}, {"Title": "Deep Work"}];
 
 // Constructor function
 function Book(){
@@ -8,14 +8,27 @@ function Book(){
 function addBookToLibrary(){
     let bookName = prompt("title:")
     let bookAuthor = prompt("author:")
-    
     let book1 = {
         "Title": bookName,
         "Author": bookAuthor,
     }
     myBook.push(book1)
+    /*const newBook = "<h1>" + `${book1.Title}` + "</h1>"
+    container.insertAdjacentHTML("beforeend", newBook)*/
+    displayBooks(book1)
+}
+
+function displayBooks(e){
+    container.innerHTML += "<h1>" + `${e.Title}` + "</h1>"
 }
 
 let addBtn = document.querySelector("#btn-add")
+let container = document.querySelector(".books-container")
 
 addBtn.addEventListener("click", addBookToLibrary)
+
+
+//List the default books in the myBook array when the page loads
+for(let i = 0; i < myBook.length; i++){
+    container.innerHTML += "<h1>" + `${myBook[i].Title}` + "</h1>"
+}
