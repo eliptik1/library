c7b219d28eee9f0d75b77207d04de8020ec2a11f
eliptik <adamfeyk5@gmail.com>
2023-04-11 18:21:58 +0300

    Add read status check & update data-index assignment

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let myBook = [];
 
 // Constructor function
-function Book(title, author){
+function Book(title, author, read){
         this.Title = title;
         this.Author = author;
+        this.Read = read;
 }
 
 function modalOn(){
@@ -14,24 +15,28 @@ function modalOn(){
 function addBookToLibrary(){
     let bookName = titleInput.value
     let bookAuthor = authorInput.value
-    let book1 = new Book(bookName, bookAuthor)
+    let isRead = isReadInput.checked
+    let book1 = new Book(bookName, bookAuthor, isRead)
     myBook.push(book1)
     displayBooks(book1)
 }
 
 function displayBooks(e){
-    container.innerHTML += `<h1 class="book-card">Title: ${e.Title}<br> Author: ${e.Author}<br> <button class="removeBtn">Remove</button></h1>`;
+    container.innerHTML += `<h1 class="book-card">Title: ${e.Title}<br> Author: ${e.Author}<br> Read: ${e.Read}<br> <button class="readBtn">Read</button> <br> <button class="removeBtn">Remove</button></h1>`;
     let cards = document.querySelectorAll(".book-card")
-    cards.forEach((card, index) => card.setAttribute("data-index", index))
+    cards.forEach((card, index) => {
+        card.setAttribute("data-index", index)
+        if(myBook[index].Read === true){
+            card.classList.add("read")
+        }
+    })
+
     let removeBtns = document.querySelectorAll(".removeBtn")
-    removeBtns.forEach((btn) => {
+    removeBtns.forEach((btn, index) => {
         btn.addEventListener("click", (el)=> { 
             let parent = el.target.closest(".book-card") //Find the closest element that matches the selector ".book-card" (h1)
             parent.remove();
-            let cards = document.querySelectorAll(".book-card") //Re-define the cards nodeList after deletion of a card,
-            cards.forEach((card, index) => card.setAttribute("data-index", index)) //Re-assign index numbers.
-            let bookIndex = parent.getAttribute("data-index")
-            myBook.splice(bookIndex, 1)
+            myBook.splice(index, 1)
         })
     })   
 }
@@ -43,6 +48,7 @@ let modal = document.querySelector(".modal")
 let overlay = document.querySelector("#overlay")
 let titleInput = document.getElementById("title")
 let authorInput = document.getElementById("author")
+let isReadInput = document.getElementById("read")
 
 addBtn.addEventListener("click", modalOn)
 
